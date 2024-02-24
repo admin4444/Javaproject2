9daf6b131ca277bc2abb94a983cde4a40ae33be4
Shivanand K <shivanand.k@straumann.com>
2024-02-24 23:16:10 +0530

    hdgdh

--- src/pac/Class1.java
+++ src/pac/Class1.java
@@ -3,8 +3,8 @@ package pac;
 public class Class1 {
 
 	public static void main(String[] args) {
-		System.out.print("hiii");
-		System.out.print("hiii");
+		System.out.print("new branch");
+		System.out.print("new branchh");
 	}
 
 }
